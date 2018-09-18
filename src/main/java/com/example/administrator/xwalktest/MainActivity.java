1c7bc24bd53780fd2d0d8d3f3239ace2a1becf99
fanyanbo <fanyanbo@skyworth.com>
2018-09-18 17:08:26 +0800

    更新代码 1.增加interface 2.对接加载进度 3.把支付改成测试地址

--- src/main/java/com/example/administrator/xwalktest/MainActivity.java
+++ src/main/java/com/example/administrator/xwalktest/MainActivity.java
@@ -78,7 +78,8 @@ public class MainActivity extends Activity {
             @Override
             public void onClick(View v) {
 
-                String urlcontent = "http://beta.webapp.skysrt.com/appstore/webxtest/test7/test.html";
+//                String urlcontent = "http://beta.webapp.skysrt.com/appstore/webxtest/test7/test.html";
+                String urlcontent = "http://beta.webapp.skysrt.com/games/test/test.html";
                 Log.i(TAG,"onClick!!! url = " + urlcontent);
                 if( urlcontent != null && ( urlcontent.startsWith("http://") || urlcontent.startsWith("https://")))
                 {
